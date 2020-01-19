5277a24c908586fbe580cfe304cd0db876f2adbb
coord.e <me@coord-e.com>
2020-01-19 04:36:25 +0000

    Change: Change the order of optimizations

--- src/main.c
+++ src/main.c
@@ -48,10 +48,10 @@ static char* read_file(const char* path) {
 int main(int argc, char** argv) {
   char* input = read_file(argv[1]);
   OIR* ir     = parse(input);
-  data_flow_available_expression(ir);
-  optimization_common_subexpression_elimination(ir);
   data_flow_reaching_definition(ir);
   optimization_propagation(ir);
+  data_flow_available_expression(ir);
+  optimization_common_subexpression_elimination(ir);
   data_flow_liveness(ir);
   optimization_dead_code_elimination(ir);
   print_OIR(stdout, ir);
